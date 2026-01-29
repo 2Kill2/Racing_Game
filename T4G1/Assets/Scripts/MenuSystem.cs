ad0d94995d5da992b1abb9423a543bd716c7bb5a
HuneyDev <64155388+2Kill2@users.noreply.github.com>
2026-01-29 14:44:25 -0800

    Add main menu scene and menu system script
    Introduced MainMenu.unity scene with UI elements for starting the game, accessing settings, and selecting tracks. Added MenuSystem.cs script to handle menu interactions and scene transitions. Updated EditorBuildSettings to include the new scene and modified the solution file accordingly.

---
+++ T4G1/Assets/Scripts/MenuSystem.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSystem : MonoBehaviour
+{
+    [Header("Menu Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject playPanel;
+    public GameObject settingsPanel;
+    public GameObject creditsPanel;
+
+    public void OnPlayButton()
+    {
+        mainMenuPanel.SetActive(false);
+        playPanel.SetActive(true);
+    }
+
+    public void OnSettingsButton()
+    {
+        mainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void OnCreditsButton()
+    {
+        mainMenuPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void OnBackToMainMenuButton()
+    {
+        playPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void OnQuitButton()
+    {
+        Application.Quit();
+        Debug.Log("Quit button pressed. Application will close.");
+    }
+
+    //TRACKS
+
+    public enum Track
+    {
+        Germany,
+        Italy,
+        Japan,
+        USA
+    }
+
+    public void LoadTrack(int trackIndex)
+    {
+        Track SelectedTrack = (Track)trackIndex;
+        SceneManager.LoadScene(SelectedTrack.ToString());
+    }
+    
+}
